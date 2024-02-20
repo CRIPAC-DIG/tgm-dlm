f45cffb58ad7b60697a29fc8bb4bd5105d8bf20c
Deno_V <56070660+Deno-V@users.noreply.github.com>
2024-02-20 20:00:13 +0800

    Delete improved-diffusion directory

--- improved-diffusion/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="improved-diffusion",
-    py_modules=["improved_diffusion"],
-    install_requires=["blobfile", "torch", "tqdm"],
-    #install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
-)
