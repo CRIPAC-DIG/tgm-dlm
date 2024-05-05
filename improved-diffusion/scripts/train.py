85910310cf0eb5ecf2f3095082a12382dab7ec9d
Deno_V <56070660+Deno-V@users.noreply.github.com>
2024-05-05 20:32:56 +0800

    Lower the Learning rate

--- improved-diffusion/scripts/train.py
+++ improved-diffusion/scripts/train.py
@@ -152,7 +152,8 @@ def create_argparser():
         learn_sigma=False, 
         log_interval=20, 
         logits_mode=1, 
-        lr=0.0001, 
+        lr = 0.00005
+        # lr=0.0001, # Lower the learing rate in 2024/5/5 for better convergence 
         lr_anneal_steps=200000, 
         microbatch=-1, 
         modality='e2e-tgt', 
